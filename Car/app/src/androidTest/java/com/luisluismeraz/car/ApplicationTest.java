7e62efd1cc493e78e9b5fafd92f1886ca60b2593
Luis Meraz <lgmeraz@live.com>
2015-03-27 01:11:46 -0700

    Map SDK

--- Car/app/src/androidTest/java/com/luisluismeraz/car/ApplicationTest.java
+++ Car/app/src/androidTest/java/com/luisluismeraz/car/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.luisluismeraz.myapplication;
+package com.luisluismeraz.car;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- Car/app/src/main/java/com/luisluismeraz/car/DisplayMessageActivity.java
+++ Car/app/src/main/java/com/luisluismeraz/car/DisplayMessageActivity.java
@@ -1,25 +1,29 @@
-package com.luisluismeraz.myapplication;
+package com.luisluismeraz.car;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.content.Intent   ;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
+        //Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //set the view as the activity layout
+        setContentView(textView);
     }
 
     @Override
